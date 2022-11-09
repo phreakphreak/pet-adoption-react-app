5ca72affc5206827d8f69153f0dc6d05d4031da7
Michael Zabala(Mike) <wonkaphreak@gmail.com>
2022-11-08 23:22:53 -0400

    feat: :zap: add  TailwindCSS to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App(): JSX.Element {
-  return <div className="App"></div>;
+  return (
+    <div className="bg-slate-900 h-screen w-screen text-white text-7xl font-bold">
+      Hello World! Welcome Pet Adoption App
+    </div>
+  );
 }
 
 export default App;
